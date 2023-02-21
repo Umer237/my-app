787140b3456862eddd54c2def5d6a6049ba8cc6a
Umer237 <umerafzal981@gmail.com>
2023-02-21 17:39:42 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Header from "./Header";
 import Home from "./Home";
 
 function App() {
   return (
     <>
  
-    <Header/>
   <Switch>  
     <Route exact path="/" component={Home}/>
-
     <Redirect to="/" />
   </Switch>
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import Section2 from './Section2'
+import Header from './Header'
 import Banner from './Banner'
+import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
 import Section5 from './Section5'
 import Section6 from './Section6'
+import Footer from './Footer'
 import EndFooter from './EndFooter'
+
 
 const Home = () => {
   return (
   <>
+  <Header/>
   <Banner/>
   <Section2/>
   <Section3/>
   <Section4/>
   <Section5/>
   <Section6/>
+  <Footer/>
   <EndFooter/>
   </>
   )
