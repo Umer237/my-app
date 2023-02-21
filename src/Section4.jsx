f6242e15de2d7c373926b0fe370f7e50c3803b7d
Umer237 <umerafzal981@gmail.com>
2023-02-21 12:58:58 +0500

    update

--- src/Section4.jsx
+++ src/Section4.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import './Section4.css'
 import Travel from './images/Travel.jpg' 
 import Girl from './images/Girl.jpeg'
-import { BsPlus } from "react-icons/Bs";
+import Alexander from './images/Alexander.jpg'
+import Nicolas from './images/Nicolas.jpg'
+import Egypt from './images/Egypt-Women.jpeg'
+import { BsPlus } from "react-icons/bs";
 
 const Section4 = () => {
   return (
@@ -13,10 +16,10 @@ const Section4 = () => {
     <h3>Know the people you,re going to meet</h3>
     <h4>Have a quick conversation anytime you need with your fellow travellers you,re going to travel with. </h4>
     <img className='Contact' src={Girl} alt="" />
-    <img className='Contact' src={Girl} alt="" />
-    <img className='Contact' src={Girl} alt="" />
-    <img className='Contact' src={Girl} alt="" />
-    <BsPlus/>
+    <img className='Contact' src={Alexander} alt="" />
+    <img className='Contact' src={Nicolas} alt="" />
+    <img className='Contact' src={Egypt} alt="" />
+    <BsPlus className='Plus'/>
     <p>with one simple click you can know who you can have as your travel buddy.</p>
     </div>
     <div className='image'>
