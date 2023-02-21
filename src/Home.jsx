55032c6df1a3b63d7e0a229e48bb3d5611e5964e
Umer237 <umerafzal981@gmail.com>
2023-02-21 13:02:35 +0500

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Section2 from './Section2'
 import Banner from './Banner'
 import Section3 from './Section3'
-import Team from './Team'
 import Section4 from './Section4'
 
 const Home = () => {
@@ -10,7 +9,6 @@ const Home = () => {
   <>
   <Banner/>
   <Section2/>
-  <Team/>
   <Section3/>
   <Section4/>
   </>
